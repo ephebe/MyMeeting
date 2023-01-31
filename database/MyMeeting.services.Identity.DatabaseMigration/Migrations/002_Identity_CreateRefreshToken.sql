7a99fb2645191ea70c034cde7b94e8fe21fa620e
Hugo <hugo@super-royal.com>
2023-01-31 18:02:16 +0800

    開發中

--- database/MyMeeting.services.Identity.DatabaseMigration/Migrations/002_Identity_CreateRefreshToken.sql
+++ database/MyMeeting.services.Identity.DatabaseMigration/Migrations/002_Identity_CreateRefreshToken.sql
@@ -2,7 +2,10 @@
     [id]                   UNIQUEIDENTIFIER NOT NULL,
     [token]                 VARCHAR (100)   NOT NULL,
     [user_id]               UNIQUEIDENTIFIER     NOT NULL,
-    CONSTRAINT [pk_identity_refresh_tokens_id] PRIMARY KEY CLUSTERED ([id] ASC)
+    CONSTRAINT [pk_identity_refresh_tokens_id] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT FK_refresh_tokens_AspNetUsers FOREIGN KEY (user_id)
+        REFERENCES AspNetUsers (Id)
+        ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX idx_token_user_id ON dbo.refresh_tokens (token ASC, user_id ASC);
---
+++ src/services/Identity/MyMeeting.Services.Identity.Application/Users/RegisteringUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Abstractions.CQRS.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMeeting.Services.Identity.Application.Users.RegisteringUser;
+
+public class RegisterUserCommandHandler : ICommandHandler<RegisterUserCommand, RegisterUserResult>
+{
+    public Task<RegisterUserResult> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
